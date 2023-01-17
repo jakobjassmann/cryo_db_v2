dc22f143f50ef5062ed263ab2f54336102aa7a1b
jakobjassmann <jakob.assmann@uzh.ch>
2023-01-17 14:16:04 +0100

    added basic scripts and folder structure

---
+++ cryo_db/fetch_cryo_data.R
@@ -0,0 +1,34 @@
+# Fetch cryo db data for the tabular version of the holocene arctic biodiversity
+# map
+# Jakob J. Assmann jakob.assmann@uzh.ch 17 January 2023
+
+## Prepare envrionment ----
+cat("Loading packages...")
+
+# List of packages
+required_packages <-
+  c("terra")
+
+# Attempt load
+packages_loaded <- sapply(required_packages, require, character.only = T)
+
+# Check loading
+if(allTrue(packages_loaded)){
+  cat("... all packages loaded.\n")
+} else {
+  # If not successful install and load packages that failed
+  for(i in length(packages_loaded)){
+    if(!packages_loaded[i]){
+      cat("\nInstalling package", required_packages[i])
+      install.packages(required_packages[i])
+      library(required_packages[i])
+      cat("Success.")
+    }
+  }
+  cat("... all packages installed and loaded.\n")
+}
+
+## Load site coordinates from ahbddb.txt
+locations <- read.csv("")
+
+test_rast <- rast("/vsicurl/https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/orog/CHELSA_TraCE21k_dem_-100_V1.0.tif")
